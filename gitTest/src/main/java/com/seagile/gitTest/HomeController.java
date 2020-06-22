b2a57edaf9c80fd57154da7152dd559ba5dfcd5d
ILOVOK <ILOVOK@192.168.1.12>
2020-06-22 09:46:34 +0900

    참가했어용

--- gitTest/src/main/java/com/seagile/gitTest/HomeController.java
+++ gitTest/src/main/java/com/seagile/gitTest/HomeController.java
@@ -16,26 +16,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
 		model.addAttribute("serverTime", formattedDate );
 		System.out.println("gitTest");
 		System.out.println("gitTest2");
 		System.out.println("gitTest3");
+		System.out.println("gitTest4444");
 		return "home";
 	}
-	
+
 }
